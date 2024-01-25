dd6d7be100add5862008183bdca1be9e8d9b1816
PabloNovaes <ppablo.0028922@gmail.com>
2024-01-25 15:43:49 -0300

    feat: update animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { SearchBar } from "./components/SearchBar";
 import { MagicMotion } from "react-magic-motion";
 export function App() {
   return (
-    <MagicMotion layoutDependency={true}>
+    <MagicMotion transition={{ type: "spring", stiffness: 200, damping: 15 }}>
       <SearchBar />
     </MagicMotion>
   );
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,9 +5,7 @@ import "../styles/repositories.scss";
 export function RepositoryList({ repositories }) {
   return (
     <ul>
-      {repositories.length <= 0 ? (
-        <h1>Nenhum resultado</h1>
-      ) : (
+      {repositories.length != 0 &&  (
         repositories.map((repo) => {
           return <RepositoryItem key={repo.name} repository={repo} />;
         })
